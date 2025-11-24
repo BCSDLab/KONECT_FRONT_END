1074d280a70e55b72b0a08c6252e15089b8a8f42
이준영 <ff1451@gmail.com>
2025-11-24 21:23:01 +0900

    chore: styled-components 제거 및 tailwind 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import GlobalStyle from './styles/GlobalStyle';
 
 function App() {
   return (
     <BrowserRouter>
-      <GlobalStyle />
       <Routes>
         <Route path="/" element={<div>Home</div>} />
       </Routes>
