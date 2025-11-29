292fb9563b2718b49d7a423865a3ad394d1bb0e6
이준영 <ff1451@gmail.com>
2025-11-30 00:02:20 +0900

    fix: 타입 수정

--- src/utils/hooks/useDebounce.ts
+++ src/utils/hooks/useDebounce.ts
@@ -1,44 +1,44 @@
-// import { useEffect, useLayoutEffect, useMemo, useRef } from 'react';
-// import type { DependencyList, EffectCallback } from 'react';
+import { useEffect, useLayoutEffect, useMemo, useRef } from 'react';
+import type { DependencyList, EffectCallback } from 'react';
 
-// function useIsomorphicLayoutEffect(effect: EffectCallback, deps?: DependencyList) {
-//   const effectHook = typeof window !== 'undefined' ? useLayoutEffect : useEffect;
+function useIsomorphicLayoutEffect(effect: EffectCallback, deps?: DependencyList) {
+  const effectHook = typeof window !== 'undefined' ? useLayoutEffect : useEffect;
 
-//   effectHook(effect, deps);
-// }
+  effectHook(effect, deps);
+}
 
-// const DEBOUNCE_DEFAULT_TIME = 300;
+const DEBOUNCE_DEFAULT_TIME = 300;
 
-// function useDebouncedCallback<F extends (...args: unknown[]) => ReturnType<F>>(
-//   callback: F,
-//   debounceTime: number = DEBOUNCE_DEFAULT_TIME
-// ) {
-//   const timer = useRef<NodeJS.Timeout | null>(null);
-//   const callbackRef = useRef(callback);
+function useDebouncedCallback<F extends (...args: unknown[]) => ReturnType<F>>(
+  callback: F,
+  debounceTime: number = DEBOUNCE_DEFAULT_TIME
+) {
+  const timer = useRef<ReturnType<typeof setTimeout> | null>(null);
+  const callbackRef = useRef(callback);
 
-//   useIsomorphicLayoutEffect(() => {
-//     callbackRef.current = callback;
-//   }, [callback]);
+  useIsomorphicLayoutEffect(() => {
+    callbackRef.current = callback;
+  }, [callback]);
 
-//   useIsomorphicLayoutEffect(() => {
-//     if (timer.current) {
-//       clearTimeout(timer.current);
-//     }
-//   }, []);
+  useIsomorphicLayoutEffect(() => {
+    if (timer.current) {
+      clearTimeout(timer.current);
+    }
+  }, []);
 
-//   return useMemo(
-//     () =>
-//       (...params: Parameters<F>) => {
-//         if (timer.current) {
-//           clearTimeout(timer.current);
-//         }
+  return useMemo(
+    () =>
+      (...params: Parameters<F>) => {
+        if (timer.current) {
+          clearTimeout(timer.current);
+        }
 
-//         timer.current = setTimeout(() => {
-//           callbackRef.current(...params);
-//         }, debounceTime);
-//       },
-//     [debounceTime]
-//   );
-// }
+        timer.current = setTimeout(() => {
+          callbackRef.current(...params);
+        }, debounceTime);
+      },
+    [debounceTime]
+  );
+}
 
-// export default useDebouncedCallback;
+export default useDebouncedCallback;
