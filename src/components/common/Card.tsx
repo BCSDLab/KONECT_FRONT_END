8b8a082a50f7778f41348faae604a3807c57678d
이준영 <ff1451@gmail.com>
2025-12-01 18:22:00 +0900

    feat: 라이브러리 추가 및 카드 컴포넌트 적용

--- src/components/common/Card.tsx
+++ src/components/common/Card.tsx
@@ -1,5 +1,19 @@
-function Card({ children }: { children: React.ReactNode }) {
-  return <div className="border-indigo-5 flex w-full flex-col gap-3 rounded-lg border bg-white p-3">{children}</div>;
+import type { HTMLAttributes, ReactNode } from 'react';
+import clsx from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+interface CardProps extends HTMLAttributes<HTMLDivElement> {
+  children: ReactNode;
+}
+
+function Card({ children, className, ...props }: CardProps) {
+  const base = 'border-indigo-5 flex w-full flex-col gap-3 rounded-lg border bg-white p-3';
+
+  return (
+    <div className={twMerge(clsx(base, className))} {...props}>
+      {children}
+    </div>
+  );
 }
 
 export default Card;
