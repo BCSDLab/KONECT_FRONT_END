a030b1d95b936854f96dbbeaf1bd4914ca391715
이준영 <ff1451@gmail.com>
2025-12-12 21:30:22 +0900

    feat: 회원가입 페이지 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AppLayout, HomeLayout } from './components/Layout';
+import { AppLayout, AuthLayout, HomeLayout } from './components/Layout';
 import ClubDetail from './pages/ClubDetail';
 import ClubList from './pages/ClubList';
 import ClubSearch from './pages/ClubSearch';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import FinishStep from './pages/SignUp/FinishStep';
+import NameStep from './pages/SignUp/NameStep';
+import SchoolStep from './pages/SignUp/SchoolStep';
+import StudentIdStep from './pages/SignUp/StudentIdStep';
+import TermStep from './pages/SignUp/TermStep';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route element={<AuthLayout />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup">
+            <Route index element={<TermStep />} />
+            <Route path="profile">
+              <Route path="school" element={<SchoolStep />} />
+              <Route path="student-id" element={<StudentIdStep />} />
+              <Route path="name" element={<NameStep />} />
+            </Route>
+            <Route path="finish" element={<FinishStep />} />
+          </Route>
+        </Route>
         <Route element={<HomeLayout />}>
           <Route path="/" element={<Home />} />
         </Route>
---
+++ src/pages/SignUp/FinishStep.tsx
@@ -0,0 +1,33 @@
+import { useNavigate } from 'react-router-dom';
+import CheckInCircleIcon from '@/assets/svg/check-in-circle.svg';
+
+function FinishStep() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex flex-1 flex-col justify-between px-8">
+      <div className="flex flex-col items-center text-center">
+        <CheckInCircleIcon className="mt-15" />
+
+        <div className="mt-8 text-2xl font-extrabold">환영합니다!</div>
+
+        <div>
+          <div className="font-extrabold">공우진님</div>
+          <div className="mt-1 text-xs text-indigo-300">
+            <div>KONECT 가입이 완료되었습니다.</div>
+            <div>동아리 활동을 시작해보세요!</div>
+          </div>
+        </div>
+      </div>
+
+      <button
+        onClick={() => navigate('/')}
+        className="bg-primary text-indigo-0 mb-8 h-12 items-center rounded-lg font-extrabold"
+      >
+        시작하기
+      </button>
+    </div>
+  );
+}
+
+export default FinishStep;
