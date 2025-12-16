fb8f1c7e7215571384e9f3b7c0878faff1a012b2
이준영 <ff1451@gmail.com>
2025-12-16 18:14:08 +0900

    fix: 버그 수정

--- src/pages/SignUp/hooks/useSignup.ts
+++ src/pages/SignUp/hooks/useSignup.ts
@@ -1,8 +1,14 @@
 import { useMutation } from '@tanstack/react-query';
 import { postSignup } from '@/apis/auth';
+import type { ApiError } from '@/apis/client';
 
 export const useSignupMutation = () => {
-  return useMutation({
+  const mutation = useMutation({
     mutationFn: postSignup,
   });
+
+  return {
+    ...mutation,
+    error: mutation.error as ApiError | null,
+  };
 };
