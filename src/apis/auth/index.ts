a94783ec91c757c23907fe80327f8199250231c1
이준영 <ff1451@gmail.com>
2025-12-28 15:53:32 +0900

    refactor: 불필요한 슬래시 제거

--- src/apis/auth/index.ts
+++ src/apis/auth/index.ts
@@ -2,7 +2,7 @@ import { apiClient } from '../client';
 import type { ModifyMyInfoRequest, MyInfoResponse, SignupRequest } from './entity';
 
 export const signup = async (data: SignupRequest) => {
-  const response = await apiClient.post('/users/signup', {
+  const response = await apiClient.post('users/signup', {
     body: data,
     requiresAuth: true,
   });
@@ -10,21 +10,21 @@ export const signup = async (data: SignupRequest) => {
 };
 
 export const getMyInfo = async () => {
-  const response = await apiClient.get<MyInfoResponse>('/users/me', {
+  const response = await apiClient.get<MyInfoResponse>('users/me', {
     requiresAuth: true,
   });
   return response;
 };
 
 export const logout = async () => {
-  const response = await apiClient.post('/users/logout', {
+  const response = await apiClient.post('users/logout', {
     requiresAuth: true,
   });
   return response;
 };
 
 export const putMyInfo = async (data: ModifyMyInfoRequest) => {
-  const response = await apiClient.put('/users/me', {
+  const response = await apiClient.put('users/me', {
     body: data,
     requiresAuth: true,
   });
@@ -32,7 +32,7 @@ export const putMyInfo = async (data: ModifyMyInfoRequest) => {
 };
 
 export const deleteMyAccount = async () => {
-  const response = await apiClient.delete('/users/withdraw', {
+  const response = await apiClient.delete('users/withdraw', {
     requiresAuth: true,
   });
   return response;
