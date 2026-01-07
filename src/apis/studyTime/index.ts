928dccdd3834fc2c9a11cb0c6f71317c50bb318a
이준영 <ff1451@gmail.com>
2026-01-07 17:17:30 +0900

    feat: 인증 여부 추가

--- src/apis/studyTime/index.ts
+++ src/apis/studyTime/index.ts
@@ -2,17 +2,20 @@ import { apiClient } from '../client';
 import type { TimerOffResponse, StudyTimeSummaryResponse } from './entity';
 
 export const getStudyTimeSummary = async () => {
-  console.log('[API] getStudyTimeSummary called');
-  const response = await apiClient.get<StudyTimeSummaryResponse>('studytimes/summary');
+  const response = await apiClient.get<StudyTimeSummaryResponse>('studytimes/summary', {
+    requiresAuth: true,
+  });
   return response;
 };
 
-export const StartStudyTimer = async () => {
-  const response = await apiClient.post('studytimes/timers');
+export const startStudyTimer = async () => {
+  const response = await apiClient.post('studytimes/timers', {
+    requiresAuth: true,
+  });
   return response;
 };
 
-export const StopStudyTimer = async (data: { totalSeconds: number }) => {
-  const response = await apiClient.delete<TimerOffResponse>('studytimes/timers', { body: data });
+export const stopStudyTimer = async (data: { totalSeconds: number }) => {
+  const response = await apiClient.delete<TimerOffResponse>('studytimes/timers', { requiresAuth: true, body: data });
   return response;
 };
