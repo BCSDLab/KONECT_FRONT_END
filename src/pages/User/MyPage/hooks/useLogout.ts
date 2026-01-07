aa725d2e7125e74fd9dfa42648b5a17bbbd07c2e
Lee Junyoung <54898597+ff1451@users.noreply.github.com>
2026-01-07 18:30:13 +0900

    [feat] 자동 로그인 구현 (#34)
    * feat: 인증 저장소 구현
    
    * feat: wrapper 구현
    
    * refactor: 구조 변경, 로그아웃 로직 수정 등

--- src/pages/User/MyPage/hooks/useLogout.ts
+++ src/pages/User/MyPage/hooks/useLogout.ts
@@ -1,13 +1,16 @@
 import { useMutation } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 import { logout } from '@/apis/auth';
+import { useAuthStore } from '@/stores/authStore';
 
 export const useLogoutMutation = () => {
   const navigate = useNavigate();
+  const clearAuth = useAuthStore((state) => state.clearAuth);
 
   return useMutation({
     mutationFn: logout,
     onSuccess: () => {
+      clearAuth();
       navigate('/');
     },
   });
