7188da7fb91acd0a577f6038fc1963bea91df638
이준영 <ff1451@gmail.com>
2026-01-17 01:01:42 +0900

    fix: viewport 조정

--- src/utils/hooks/useKeyboardHeight.ts
+++ src/utils/hooks/useKeyboardHeight.ts
@@ -3,20 +3,22 @@ import { useEffect } from 'react';
 function useKeyboardHeight() {
   useEffect(() => {
     const visualViewport = window.visualViewport;
-    if (!visualViewport) return;
 
     const setViewportHeight = () => {
-      document.documentElement.style.setProperty('--viewport-height', `${visualViewport.height}px`);
+      const height = visualViewport?.height ?? window.innerHeight;
+      document.documentElement.style.setProperty('--viewport-height', `${height}px`);
     };
 
     setViewportHeight();
 
-    visualViewport.addEventListener('resize', setViewportHeight);
-    visualViewport.addEventListener('scroll', setViewportHeight);
+    visualViewport?.addEventListener('resize', setViewportHeight);
+    visualViewport?.addEventListener('scroll', setViewportHeight);
+    window.addEventListener('resize', setViewportHeight);
 
     return () => {
-      visualViewport.removeEventListener('resize', setViewportHeight);
-      visualViewport.removeEventListener('scroll', setViewportHeight);
+      visualViewport?.removeEventListener('resize', setViewportHeight);
+      visualViewport?.removeEventListener('scroll', setViewportHeight);
+      window.removeEventListener('resize', setViewportHeight);
     };
   }, []);
 }
