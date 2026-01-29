8a058adb08ffea54c0439dc633b8d94499ec1ef1
이준영 <ff1451@gmail.com>
2026-01-29 20:53:41 +0900

    fix: lint 에러 수정

--- src/apis/schedule/index.ts
+++ src/apis/schedule/index.ts
@@ -2,7 +2,10 @@ import { apiClient } from '../client';
 import type { ScheduleListResponse, ScheduleRequestParams } from './entity';
 
 export const getScheduleList = async (params: ScheduleRequestParams) => {
-  const response = await apiClient.get<ScheduleListResponse, ScheduleRequestParams>('schedules', { params, requiresAuth: true });
+  const response = await apiClient.get<ScheduleListResponse, ScheduleRequestParams>('schedules', {
+    params,
+    requiresAuth: true,
+  });
   return response;
 };
 
