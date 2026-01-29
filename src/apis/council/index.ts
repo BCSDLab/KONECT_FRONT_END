3dd3d82469c5f286f210bb96fa6549588ba10666
이준영 <ff1451@gmail.com>
2026-01-29 20:52:11 +0900

    feat: 토큰 추가

--- src/apis/council/index.ts
+++ src/apis/council/index.ts
@@ -2,16 +2,16 @@ import { apiClient } from '../client';
 import type { CouncilNoticeDetail, CouncilResponse, NoticeRequestParams, NoticeResponse } from './entity';
 
 export const getCouncilNotice = async (params: NoticeRequestParams) => {
-  const response = await apiClient.get<NoticeResponse, NoticeRequestParams>('councils/notices', { params });
+  const response = await apiClient.get<NoticeResponse, NoticeRequestParams>('councils/notices', { params, requiresAuth: true });
   return response;
 };
 
 export const getCouncilInfo = async () => {
-  const response = await apiClient.get<CouncilResponse>('councils');
+  const response = await apiClient.get<CouncilResponse>('councils', { requiresAuth: true });
   return response;
 };
 
 export const getCouncilNoticeDetail = async (noticeId: number) => {
-  const response = await apiClient.get<CouncilNoticeDetail>(`councils/notices/${noticeId}`);
+  const response = await apiClient.get<CouncilNoticeDetail>(`councils/notices/${noticeId}`, { requiresAuth: true });
   return response;
 };
--- src/apis/schedule/index.ts
+++ src/apis/schedule/index.ts
@@ -2,11 +2,11 @@ import { apiClient } from '../client';
 import type { ScheduleListResponse, ScheduleRequestParams } from './entity';
 
 export const getScheduleList = async (params: ScheduleRequestParams) => {
-  const response = await apiClient.get<ScheduleListResponse, ScheduleRequestParams>('schedules', { params });
+  const response = await apiClient.get<ScheduleListResponse, ScheduleRequestParams>('schedules', { params, requiresAuth: true });
   return response;
 };
 
 export const getUpComingScheduleList = async () => {
-  const response = await apiClient.get<ScheduleListResponse>('schedules/upcoming');
+  const response = await apiClient.get<ScheduleListResponse>('schedules/upcoming', { requiresAuth: true });
   return response;
 };
