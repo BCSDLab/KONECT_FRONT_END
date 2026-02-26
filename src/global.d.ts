0132e8be021918f8808996a36287114d4997465d
이준영 <54898597+ff1451@users.noreply.github.com>
2026-02-27 02:11:46 +0900

    [feat] sentry 모니터링 추가 (#141) (#142)
    * chore: sentry 의존성 추가
    
    * feat: sentry 세팅
    
    * chore: 워크플로우 수정 및 vite 세팅
    
    * chore: 임시 추가

--- src/global.d.ts
+++ src/global.d.ts
@@ -3,3 +3,19 @@ interface Window {
     postMessage: (message: string) => void;
   };
 }
+
+interface ImportMetaEnv {
+  readonly VITE_API_PATH: string;
+  readonly VITE_SENTRY_DSN?: string;
+  readonly VITE_SENTRY_ENABLED?: 'true' | 'false';
+  readonly VITE_SENTRY_ENVIRONMENT?: string;
+  readonly VITE_SENTRY_RELEASE?: string;
+  readonly VITE_SENTRY_TRACES_SAMPLE_RATE?: string;
+  readonly VITE_SENTRY_REPLAY_SESSION_SAMPLE_RATE?: string;
+  readonly VITE_SENTRY_REPLAY_ON_ERROR_SAMPLE_RATE?: string;
+  readonly VITE_SENTRY_DEBUG_TRANSACTIONS?: 'true' | 'false';
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
