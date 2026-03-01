2042f168c445f8af6e85e7ca91839448f20edd74
이준영 <54898597+ff1451@users.noreply.github.com>
2026-03-01 19:53:55 +0900

    [chore] pwa 제거 (#148)
    * chore: pwa 제거
    
    * chore: 코드래빗 설정 강황
    
    * chore: devdependencies 분리
    
    * chore: pnpm-lock 반영

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { sentryVitePlugin } from '@sentry/vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
 import svgr from 'vite-plugin-svgr';
 
 const sentryOrg = process.env.SENTRY_ORG;
@@ -23,74 +22,6 @@ export default defineConfig({
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
-    VitePWA({
-      registerType: 'autoUpdate',
-      injectRegister: 'auto',
-      manifest: {
-        name: 'KONECT',
-        short_name: 'Konect',
-        description: '대학 동아리 문화를 하나로, 당신의 캠퍼스 라이프를 넓히는 가장 확실한 연결 : Konect',
-        id: '/',
-
-        start_url: '/',
-        scope: '/',
-        display: 'standalone',
-        orientation: 'portrait',
-        theme_color: '#ffffff',
-        background_color: '#000000',
-        icons: [
-          {
-            src: 'pwa-64x64.png',
-            sizes: '64x64',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: 'maskable-icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
-        ],
-      },
-      workbox: {
-        cleanupOutdatedCaches: true,
-        clientsClaim: true,
-        skipWaiting: true,
-
-        navigateFallbackDenylist: [/^\/api(\/|$)/],
-
-        // 런타임 캐싱: 이미지/폰트 같은 것들
-        runtimeCaching: [
-          {
-            urlPattern: ({ request }) => request.destination === 'image',
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'images',
-              expiration: { maxEntries: 100, maxAgeSeconds: 60 * 60 * 24 * 30 },
-            },
-          },
-          {
-            urlPattern: ({ request }) => request.destination === 'font',
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'fonts',
-              expiration: { maxEntries: 30, maxAgeSeconds: 60 * 60 * 24 * 365 },
-            },
-          },
-        ],
-      },
-    }),
-
     tailwindcss(),
     svgr({ include: '**/*.svg' }),
     ...(shouldUploadSourcemaps
@@ -115,8 +46,5 @@ export default defineConfig({
   },
   server: {
     port: 3000,
-  },
-  preview: {
-    port: 5000,
   },
 });
