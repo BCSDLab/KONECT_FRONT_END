c3a21fbc4ff52ddf662c069a8d7135feb673873f
이준영 <ff1451@gmail.com>
2026-03-19 19:23:43 +0900

    feat: 라우트별 lazy import 도입 및 splitting 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,52 @@
+import { lazy, Suspense } from 'react';
 import * as Sentry from '@sentry/react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import RouteLoadingFallback from '@/components/common/RouteLoadingFallback';
 import AuthGuard from './components/auth/AuthGuard';
 import ProtectedRoute from './components/auth/ProtectedRoute';
 import PublicRoute from './components/auth/PublicRoute';
 import Layout from './components/layout';
 import Login from './pages/Auth/Login';
-import ConfirmStep from './pages/Auth/SignUp/ConfirmStep';
-import FinishStep from './pages/Auth/SignUp/FinishStep';
-import NameStep from './pages/Auth/SignUp/NameStep';
-import StudentIdStep from './pages/Auth/SignUp/StudentIdStep';
-import TermStep from './pages/Auth/SignUp/TermStep';
-import UniversityStep from './pages/Auth/SignUp/UniversityStep';
-import ChatListPage from './pages/Chat';
-import ChatRoom from './pages/Chat/ChatRoom';
-import ApplicationPage from './pages/Club/Application';
-import ApplyCompletePage from './pages/Club/Application/applyCompletePage';
-import ClubFeePage from './pages/Club/Application/clubFeePage';
-import ClubDetail from './pages/Club/ClubDetail';
-import ClubList from './pages/Club/ClubList';
-import ClubSearch from './pages/Club/ClubSearch';
-import CouncilDetail from './pages/Council/CouncilDetail';
-import CouncilNotice from './pages/Council/CouncilNotice';
-import GuidePage from './pages/Guide';
 import Home from './pages/Home';
-import LicensePage from './pages/legal/LicensePage';
-import MarketingPolicyPage from './pages/legal/MarketingPolicyPage';
-import PrivacyPolicyPage from './pages/legal/PrivacyPolicyPage';
-import TermsPage from './pages/legal/TermsPage';
-import ManagedAccount from './pages/Manager/ManagedAccount';
-import ManagedApplicationDetail from './pages/Manager/ManagedApplicationDetail';
-import ManagedApplicationList from './pages/Manager/ManagedApplicationList';
-import ManagedClubDetail from './pages/Manager/ManagedClubDetail';
-import ManagedClubList from './pages/Manager/ManagedClubList';
-import ManagedClubInfo from './pages/Manager/ManagedClubProfile';
-import ManagedMemberApplicationDetail from './pages/Manager/ManagedMemberApplicationDetail';
-import ManagedMemberList from './pages/Manager/ManagedMemberList';
-import ManagedRecruitment from './pages/Manager/ManagedRecruitment';
-import ManagedRecruitmentForm from './pages/Manager/ManagedRecruitmentForm';
-import ManagedRecruitmentWrite from './pages/Manager/ManagedRecruitmentWrite';
 import NotFoundPage from './pages/NotFound';
-import Schedule from './pages/Schedule';
 import ServerErrorPage from './pages/ServerError';
-import Timer from './pages/Timer';
-import MyPage from './pages/User/MyPage';
-import Profile from './pages/User/Profile';
+
+const ConfirmStep = lazy(() => import('./pages/Auth/SignUp/ConfirmStep'));
+const FinishStep = lazy(() => import('./pages/Auth/SignUp/FinishStep'));
+const NameStep = lazy(() => import('./pages/Auth/SignUp/NameStep'));
+const StudentIdStep = lazy(() => import('./pages/Auth/SignUp/StudentIdStep'));
+const TermStep = lazy(() => import('./pages/Auth/SignUp/TermStep'));
+const UniversityStep = lazy(() => import('./pages/Auth/SignUp/UniversityStep'));
+const ChatListPage = lazy(() => import('./pages/Chat'));
+const ChatRoom = lazy(() => import('./pages/Chat/ChatRoom'));
+const ApplicationPage = lazy(() => import('./pages/Club/Application'));
+const ApplyCompletePage = lazy(() => import('./pages/Club/Application/applyCompletePage'));
+const ClubFeePage = lazy(() => import('./pages/Club/Application/clubFeePage'));
+const ClubDetail = lazy(() => import('./pages/Club/ClubDetail'));
+const ClubList = lazy(() => import('./pages/Club/ClubList'));
+const ClubSearch = lazy(() => import('./pages/Club/ClubSearch'));
+const CouncilDetail = lazy(() => import('./pages/Council/CouncilDetail'));
+const CouncilNotice = lazy(() => import('./pages/Council/CouncilNotice'));
+const GuidePage = lazy(() => import('./pages/Guide'));
+const LicensePage = lazy(() => import('./pages/legal/LicensePage'));
+const MarketingPolicyPage = lazy(() => import('./pages/legal/MarketingPolicyPage'));
+const PrivacyPolicyPage = lazy(() => import('./pages/legal/PrivacyPolicyPage'));
+const TermsPage = lazy(() => import('./pages/legal/TermsPage'));
+const ManagedAccount = lazy(() => import('./pages/Manager/ManagedAccount'));
+const ManagedApplicationDetail = lazy(() => import('./pages/Manager/ManagedApplicationDetail'));
+const ManagedApplicationList = lazy(() => import('./pages/Manager/ManagedApplicationList'));
+const ManagedClubDetail = lazy(() => import('./pages/Manager/ManagedClubDetail'));
+const ManagedClubInfo = lazy(() => import('./pages/Manager/ManagedClubProfile'));
+const ManagedClubList = lazy(() => import('./pages/Manager/ManagedClubList'));
+const ManagedMemberApplicationDetail = lazy(() => import('./pages/Manager/ManagedMemberApplicationDetail'));
+const ManagedMemberList = lazy(() => import('./pages/Manager/ManagedMemberList'));
+const ManagedRecruitment = lazy(() => import('./pages/Manager/ManagedRecruitment'));
+const ManagedRecruitmentForm = lazy(() => import('./pages/Manager/ManagedRecruitmentForm'));
+const ManagedRecruitmentWrite = lazy(() => import('./pages/Manager/ManagedRecruitmentWrite'));
+const Schedule = lazy(() => import('./pages/Schedule'));
+const Timer = lazy(() => import('./pages/Timer'));
+const MyPage = lazy(() => import('./pages/User/MyPage'));
+const Profile = lazy(() => import('./pages/User/Profile'));
 
 const SentryRoutes = Sentry.withSentryReactRouterV7Routing(Routes);
 
@@ -51,80 +54,82 @@ function App() {
   return (
     <BrowserRouter>
       <AuthGuard>
-        <SentryRoutes>
-          <Route element={<PublicRoute />}>
-            <Route element={<Layout contentClassName="bg-indigo-0" />}>
-              <Route path="/" element={<Login />} />
-              <Route path="signup">
-                <Route index element={<TermStep />} />
-                <Route path="university" element={<UniversityStep />} />
-                <Route path="studentid" element={<StudentIdStep />} />
-                <Route path="name" element={<NameStep />} />
-                <Route path="confirm" element={<ConfirmStep />} />
-              </Route>
-            </Route>
-          </Route>
-
-          <Route element={<Layout contentClassName="bg-indigo-0" />}>
-            <Route path="/signup/finish" element={<FinishStep />} />
-          </Route>
-          <Route path="/guide" element={<GuidePage />} />
-
-          <Route element={<Layout />}>
-            <Route path="legal">
-              <Route path="oss" element={<LicensePage />} />
-              <Route path="terms" element={<TermsPage />} />
-              <Route path="privacy" element={<PrivacyPolicyPage />} />
-              <Route path="marketing" element={<MarketingPolicyPage />} />
-            </Route>
-          </Route>
-
-          <Route element={<ProtectedRoute />}>
-            <Route element={<Layout showBottomNav />}>
-              <Route path="home" element={<Home />} />
-              <Route path="mypage">
-                <Route index element={<MyPage />} />
-                <Route path="manager">
-                  <Route index element={<ManagedClubList />} />
-                  <Route path=":clubId" element={<ManagedClubDetail />} />
-                  <Route path=":clubId/members" element={<ManagedMemberList />} />
-                  <Route path=":clubId/members/:userId/application" element={<ManagedMemberApplicationDetail />} />
-                  <Route path=":clubId/recruitment" element={<ManagedRecruitment />} />
-                  <Route path=":clubId/applications" element={<ManagedApplicationList />} />
-                  <Route path=":clubId/applications/:applicationId" element={<ManagedApplicationDetail />} />
+        <Suspense fallback={<RouteLoadingFallback fullScreen />}>
+          <SentryRoutes>
+            <Route element={<PublicRoute />}>
+              <Route element={<Layout contentClassName="bg-indigo-0" />}>
+                <Route path="/" element={<Login />} />
+                <Route path="signup">
+                  <Route index element={<TermStep />} />
+                  <Route path="university" element={<UniversityStep />} />
+                  <Route path="studentid" element={<StudentIdStep />} />
+                  <Route path="name" element={<NameStep />} />
+                  <Route path="confirm" element={<ConfirmStep />} />
                 </Route>
               </Route>
-              <Route path="timer" element={<Timer />} />
-              <Route path="clubs">
-                <Route index element={<ClubList />} />
-                <Route path="search" element={<ClubSearch />} />
-              </Route>
             </Route>
-            <Route element={<Layout />}>
-              <Route path="clubs/:clubId" element={<ClubDetail />} />
-              <Route path="clubs/:clubId/applications" element={<ApplicationPage />} />
-              <Route path="clubs/:clubId/fee" element={<ClubFeePage />} />
-              <Route path="clubs/:clubId/complete" element={<ApplyCompletePage />} />
-              <Route path="schedule" element={<Schedule />} />
-              <Route path="mypage/manager/:clubId/info" element={<ManagedClubInfo />} />
-              <Route path="mypage/manager/:clubId/recruitment/form" element={<ManagedRecruitmentForm />} />
-              <Route path="mypage/manager/:clubId/recruitment/write" element={<ManagedRecruitmentWrite />} />
-              <Route path="mypage/manager/:clubId/recruitment/account" element={<ManagedAccount />} />
-              <Route path="profile" element={<Profile />} />
-              <Route path="council">
-                <Route index element={<CouncilDetail />} />
-                <Route path="notice/:noticeId" element={<CouncilNotice />} />
-              </Route>
-              <Route path="chats">
-                <Route index element={<ChatListPage />} />
-                <Route path=":chatRoomId" element={<ChatRoom />} />
-              </Route>
-            </Route>
-          </Route>
 
-          <Route path="server-error" element={<ServerErrorPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </SentryRoutes>
+            <Route element={<Layout contentClassName="bg-indigo-0" />}>
+              <Route path="/signup/finish" element={<FinishStep />} />
+            </Route>
+            <Route path="/guide" element={<GuidePage />} />
+
+            <Route element={<Layout />}>
+              <Route path="legal">
+                <Route path="oss" element={<LicensePage />} />
+                <Route path="terms" element={<TermsPage />} />
+                <Route path="privacy" element={<PrivacyPolicyPage />} />
+                <Route path="marketing" element={<MarketingPolicyPage />} />
+              </Route>
+            </Route>
+
+            <Route element={<ProtectedRoute />}>
+              <Route element={<Layout showBottomNav />}>
+                <Route path="home" element={<Home />} />
+                <Route path="mypage">
+                  <Route index element={<MyPage />} />
+                  <Route path="manager">
+                    <Route index element={<ManagedClubList />} />
+                    <Route path=":clubId" element={<ManagedClubDetail />} />
+                    <Route path=":clubId/members" element={<ManagedMemberList />} />
+                    <Route path=":clubId/members/:userId/application" element={<ManagedMemberApplicationDetail />} />
+                    <Route path=":clubId/recruitment" element={<ManagedRecruitment />} />
+                    <Route path=":clubId/applications" element={<ManagedApplicationList />} />
+                    <Route path=":clubId/applications/:applicationId" element={<ManagedApplicationDetail />} />
+                  </Route>
+                </Route>
+                <Route path="timer" element={<Timer />} />
+                <Route path="clubs">
+                  <Route index element={<ClubList />} />
+                  <Route path="search" element={<ClubSearch />} />
+                </Route>
+              </Route>
+              <Route element={<Layout />}>
+                <Route path="clubs/:clubId" element={<ClubDetail />} />
+                <Route path="clubs/:clubId/applications" element={<ApplicationPage />} />
+                <Route path="clubs/:clubId/fee" element={<ClubFeePage />} />
+                <Route path="clubs/:clubId/complete" element={<ApplyCompletePage />} />
+                <Route path="schedule" element={<Schedule />} />
+                <Route path="mypage/manager/:clubId/info" element={<ManagedClubInfo />} />
+                <Route path="mypage/manager/:clubId/recruitment/form" element={<ManagedRecruitmentForm />} />
+                <Route path="mypage/manager/:clubId/recruitment/write" element={<ManagedRecruitmentWrite />} />
+                <Route path="mypage/manager/:clubId/recruitment/account" element={<ManagedAccount />} />
+                <Route path="profile" element={<Profile />} />
+                <Route path="council">
+                  <Route index element={<CouncilDetail />} />
+                  <Route path="notice/:noticeId" element={<CouncilNotice />} />
+                </Route>
+                <Route path="chats">
+                  <Route index element={<ChatListPage />} />
+                  <Route path=":chatRoomId" element={<ChatRoom />} />
+                </Route>
+              </Route>
+            </Route>
+
+            <Route path="server-error" element={<ServerErrorPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </SentryRoutes>
+        </Suspense>
       </AuthGuard>
     </BrowserRouter>
   );
--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,5 +1,6 @@
 import { Suspense, type CSSProperties } from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
+import RouteLoadingFallback from '@/components/common/RouteLoadingFallback';
 import { cn } from '@/utils/ts/cn';
 import BottomNav from './BottomNav';
 import Header from './Header';
@@ -27,11 +28,11 @@ export default function Layout({ showBottomNav = false, contentClassName }: Layo
   return (
     <div className="fixed inset-0 flex flex-col overflow-hidden" style={layoutStyle}>
       {hasHeader && <Header />}
-      <Suspense>
+      <Suspense fallback={<RouteLoadingFallback />}>
         <main
           className={cn(
             'bg-background box-border flex min-h-0 flex-1 flex-col overflow-y-auto overscroll-contain [-ms-overflow-style:none] [scrollbar-width:none] [&::-webkit-scrollbar]:hidden',
-            hasHeader && (isInfoHeader ? 'pt-15' : isManagerHeader ? 'pt-[var(--manager-header-height)]' : 'pt-11'),
+            hasHeader && (isInfoHeader ? 'pt-15' : isManagerHeader ? 'pt-(--manager-header-height)' : 'pt-11'),
             showBottomNav && 'pb-19',
             contentClassName
           )}
